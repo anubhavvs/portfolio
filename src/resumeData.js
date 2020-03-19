6be9e6a4beadb5931fcec7abeb7866f9c5eec301
anubhavvs <anubhavvsaha2001@gmail.com>
2020-03-19 15:57:10 +0530

    Initial Portfolio Deply

---
+++ src/resumeData.js
@@ -0,0 +1,59 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Anubhav Saha",
+    "username" : "@anubhavvs",
+    "role": "Full Stack Web Developer and also a Python Enthusiast",
+    "linkedinId":" anubhavvs",
+    "skypeid": "",
+    "roleDescription": " I like to design and make websites from scratch and like to learn about new technologies.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/anubhavvs/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/anubhavvs",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"http://twitter.com/anubhavvs",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Hi,i am Anubhav Saha, first year under grad student. Currently i am pursuing B.C.A degree from Institute Of Engineering And Management,Kolkata. I am a self taught Full Stack Web Developer. Right now I am trying to gather some  knowledge about Machine Learning. Taking part in competitive programming is a subject of interest for me. Other than these i love to play games in my free time.",
+    "education":[
+      {
+        "UniversityName":"Institute of Engineering & Management",
+        "specialization":"Bachelor Of Computer Application",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2022",
+        "Achievements":"Kolkata, India"
+      }
+    ],
+    "skillsDescription":"Full Stack Web Developement, Python Scripting and Flutter Android Developement.",
+    "skills":[
+      {
+        "skillname":"python"
+      },
+      {
+        "skillname":"c"
+      },
+      {
+        "skillname":"javascript"
+      },
+      {
+        "skillname":"nodejs"
+      },
+      {
+        "skillname":"reactjs"
+      },
+      {
+        "skillname":"flutter"
+      }
+    ],
+  }
+  
+  export default resumeData
